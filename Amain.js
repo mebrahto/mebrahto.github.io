bde53d96321848f37b58570062bb0800b782d386
mebrahto <50428079+mebrahto@users.noreply.github.com>
2019-07-20 22:40:32 -0300

    mebrye.com

---
+++ Amain.js
@@ -0,0 +1,21 @@
+alert("You Have A Virus");
+// var name="mebrahtom"
+confirm("Confirm youre 18 or older")
+prompt("Whats your favorite animal?")
+#square-blueviolet {
+    background-color:blueviolet;
+    border-style: ridge solid dotted dashed;
+    border-color: 10px blue solid;
+  }
+
+
+    #square-white {
+    background-color:white;
+    border-style: ridge solid dotted dashed;
+    border-color: 10px blue solid; 
+
+   # square-black {
+    background-color:black;
+    border-style: ridge solid dotted dashed;
+    border-color: 10px blue solid;
+    }
--- main.js
+++
@@ -1,4 +0,0 @@
-alert("You Have A Virus");
-// var name="mebrahtom"
-confirm("Confirm youre 18 or older")
-prompt("Whats your favorite animal?")
